88e1417ec8b09abc47f8008ef48d659e773b8de8
sabiqthottoly <sabiqt@gmail.com>
2020-12-10 01:38:07 +0530

    flatlist parallax animation

--- App.js
+++ App.js
@@ -1,12 +1,80 @@
+import * as React from 'react';
+import {
+  Animated,
+  Dimensions,
+  Image,
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+const { width, height } = Dimensions.get('screen');
+const ITEM_WIDTH = width * 0.76;
+const ITEM_HEIGHT = ITEM_WIDTH * 1.47;
+
+const images = [
+  'https://images.unsplash.com/photo-1551316679-9c6ae9dec224?w=800&q=80',
+  'https://images.unsplash.com/photo-1562569633-622303bafef5?w=800&q=80',
+  'https://images.unsplash.com/photo-1503656142023-618e7d1f435a?w=800&q=80',
+  'https://images.unsplash.com/photo-1555096462-c1c5eb4e4d64?w=800&q=80',
+  'https://images.unsplash.com/photo-1517957754642-2870518e16f8?w=800&q=80',
+  'https://images.unsplash.com/photo-1546484959-f9a381d1330d?w=800&q=80',
+  'https://images.unsplash.com/photo-1548761208-b7896a6ff225?w=800&q=80',
+  'https://images.unsplash.com/photo-1511208687438-2c5a5abb810c?w=800&q=80',
+  'https://images.unsplash.com/photo-1548614606-52b4451f994b?w=800&q=80',
+  'https://images.unsplash.com/photo-1548600916-dc8492f8e845?w=800&q=80',
+];
+const data = images.map((image, index) => ({
+  key: String(index),
+  photo: image,
+  avatar_url: `https://randomuser.me/api/portraits/women/${Math.floor(
+    Math.random() * 40
+  )}.jpg`,
+}));
 
 export default function App() {
+  const scrollX = React.useRef(new Animated.Value(0)).current
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
+      <Animated.FlatList
+        data={data}
+        horizontal
+        showHorizontalScrollIndicator={false}
+        pagingEnabled
+        onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: scrollX } } }], { useNativeDriver: true })}
+        keyExtractor={item => item.key}
+        renderItem={({ item, index }) => {
+          const inputRange = [
+            (index - 1) * width,
+            index * width,
+            (index + 1) * width
+          ]
+          const translateX = scrollX.interpolate({
+            inputRange,
+            outputRange: [-width * .7, 0, width * .7]
+          })
+          return (
+            <View style={{ width, alignItems: 'center', justifyContent: "center" }}>
+              <View style={{ shadowColor: "#000", shadowOffset: { width: 0, height: 2, }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5, borderRadius: 18, padding: 5, backgroundColor: 'white' }}>
+                <View style={{ width: ITEM_WIDTH, height: ITEM_HEIGHT, overflow: 'hidden', alignItems: 'center', borderRadius: 15 }}>
+                  <Animated.Image
+                    source={{ uri: item.photo }}
+                    style={{ width: ITEM_WIDTH, height: ITEM_HEIGHT * 1.3, resizeMode: 'cover',transform:[{translateX}] }}
+                  />
+                </View>
+                <Image
+                  source={{ uri: item.avatar_url }}
+                  style={{ height: 60, width: 60,borderRadius: 60, borderWidth: 5, borderColor: "white", position: 'absolute', bottom: -30, right: 50 }}
+                />
+              </View>
+            </View>
+          )
+        }}
+      />
+
     </View>
   );
 }
